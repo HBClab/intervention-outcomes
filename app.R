d0cfac60d69727b7ea999b4b5d66252eca44e8b1
zkhan12 <zoheb1112@gmail.com>
2020-02-13 15:31:59 -0600

    add table to plot functionality, change column width, axis labels

--- app.R
+++ app.R
@@ -1,8 +1,7 @@
 library(shiny)
 library(DT)
 
-io_table <- as.data.frame(read.csv(file="InterventionOutcomesR.csv", header=TRUE, na.strings = "",
-                                   stringsAsFactors = FALSE))
+io_table <- as.data.frame(read.csv(file="InterventionOutcomesR.csv", header=TRUE, na.strings = ""))
 
 categorical <- c("StudyName", "Include", "Design", "IndependentVariable", "IndependentType",
                  "DependentVariable", "DependentType", "DependentSubType", "LinkType",
@@ -12,6 +11,7 @@ categorical <- c("StudyName", "Include", "Design", "IndependentVariable", "Indep
 numerical <- c("TotalSampleSize", "InterventionDuration", "ExperimentalGroupN",
                "ExperimentalGroup2N", "ExperimentalGroup3N", "ControlGroupN", "MeanAge",
                "PercFemale")
+
 
 ui <- fluidPage(
   
@@ -52,23 +52,29 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   
-  output$mytable <- DT::renderDataTable({
-    DT::datatable(io_table, filter = "top", options = list(pageLength = 10, sDom  = '<"top">lrt<"bottom">ip'))
+  output$mytable <- DT::renderDT({
+    DT::datatable(io_table, filter = "top", options = list(pageLength = 10,
+                                                           sDom  = '<"top">lrt<"bottom">ip',
+                                                           scrollX=TRUE,
+                                                           autoWidth = TRUE,
+                                                           columnDefs = list(list(width = '150px', targets = c(1: 23)))
+                                                           )
+                  )
   })
 
   output$mainPlot <- renderPlot({
     
     if (input$plotType == "bar"){
       interestCol <- input$barCol
-      val <- io_table[, interestCol]
+      val <- io_table[input$mytable_rows_all, interestCol]
       tbl <- as.data.frame(table(val))
-      plt <- barplot(tbl$Freq)
+      plt <- barplot(tbl$Freq, ylab = "Frequency", xlab = interestCol)
       axis(1, at=plt, labels = tbl$val)
     }
     
     if (input$plotType == "histogram"){
       interestCol <- input$histCol
-      val <- io_table[, interestCol]
+      val <- io_table[input$mytable_rows_all, interestCol]
       numVal <- as.numeric(val)
       hist(numVal, main = "Histogram", xlab = interestCol)
     }
@@ -76,8 +82,8 @@ server <- function(input, output) {
     if (input$plotType == "box"){
       catCol <- input$boxCol1
       numCol <- input$boxCol2
-      cat <- io_table[, catCol]
-      num <- as.numeric(io_table[, numCol])
+      cat <- io_table[input$mytable_rows_all, catCol]
+      num <- as.numeric(io_table[input$mytable_rows_all, numCol])
       df <- data.frame(cat, num)
       names(df) <- c("categorical", "numerical")
       boxplot(numerical ~ categorical, data = df)
@@ -86,9 +92,9 @@ server <- function(input, output) {
     if (input$plotType == "scatter"){
       numCol1 <- input$scatterCol1
       numCol2 <- input$scatterCol2
-      num1 <- as.numeric(io_table[, numCol1])
-      num2 <- as.numeric(io_table[, numCol2])
-      plot(num1, num2, ylab = "Numerical Value 2", xlab = "Numerical Value 1")
+      num1 <- as.numeric(io_table[input$mytable_rows_all, numCol1])
+      num2 <- as.numeric(io_table[input$mytable_rows_all, numCol2])
+      plot(num1, num2, ylab = numCol2, xlab = numCol1)
     }
   })
 }
