5cc58bd27c0aa36966a04ab7147f44b7842b6cdf
zkhan12 <zoheb1112@gmail.com>
2020-06-01 15:36:33 -0500

    adjust cron params for CI

--- validation/run.sh
+++ validation/run.sh
@@ -3,6 +3,7 @@ python=/opt/miniconda-latest/envs/io/bin/python
 $python ./validation/download.py ./validation/credentials.json $1 ./tmp/
 if $python ./validation/validator.py ./tmp/intervention_data.csv ./validation/categorical_key_values.json ./app/.;
 then
+    echo "VALIDATION SUCCESSFUL"
     Rscript ./validation/deploy.R $2 $3 $4 $5 $6
 else
     echo "ERROR"
