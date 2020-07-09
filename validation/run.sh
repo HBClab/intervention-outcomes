a112b6d777ab5e33fa08786e3ef04e8ff370f138
Zoheb Khan <zoheb@mbp.local>
2020-07-09 10:18:20 -0500

    update run.sh to account for codebook file path

--- validation/run.sh
+++ validation/run.sh
@@ -10,7 +10,7 @@ APPID_DEPLOY=$5
 APPNAME_DEPLOY=$6
 
 $python ./validation/download.py ./validation/credentials.json $DOCID ./tmp/
-if $python ./validation/validator.py ./tmp/intervention_data.csv ./validation/categorical_key_values.json ./app/.;
+if $python ./validation/validator.py ./tmp/intervention_data.csv ./validation/categorical_key_values.json ./app/. ./tmp/codebook.csv;
 then
     echo "VALIDATION SUCCESSFUL"
     Rscript ./validation/deploy.R $NAME_ACC $TOKEN_ACC $SECRET_ACC $APPID_DEPLOY $APPNAME_DEPLOY
